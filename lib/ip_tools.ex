c3e574f441e7fcdf667904218604c106c24e0aa2
Chase <chase.gilliam@gmail.com>
2017-05-17 00:22:14 -0400

    httpoison version bump, minor cleanup, getting ready to publish

--- lib/ip_tools.ex
+++ lib/ip_tools.ex
@@ -1,23 +1,36 @@
 defmodule IpTools do
-  def get_ip() do
-    {ip, _} = System.cmd("dig",["+short", "myip.opendns.com", "@resolver1.opendns.com"])
+  @moduledoc """
+    Simple set of tools for finding the host machine's IP and a HTTP wrapper
+    around ipinfo.
+  """
+
+  @doc """
+    Uses `System.cmd` to call dig with the short modifier, calling "myip.opendns.com"
+  """
+  def get_ip do
+    {ip, _} = System.cmd("dig", ["+short", "myip.opendns.com", "@resolver1.opendns.com"])
     ip |> String.strip
   end
 
+  @doc """
+    Calls ipinfo.io with a provided ip address and returns a rough Lat Lon pair
+  """
   def get_ip_lat_lon(ip) do
-    HTTPoison.start
-    url = "ipinfo.io/" <> ip <> "/loc"
-    res = HTTPoison.get url
+    ip
+    |> _build_url
+    |> HTTPoison.get
+    |> clean_lat_lon_string
+  end
 
-    res
-      |> clean_lat_lon_string
+  defp _build_url(ip) do
+    "ipinfo.io/#{ip}/loc"
   end
 
   defp clean_lat_lon_string({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
     body
-      |> String.strip
-      |> String.split(",")
-      |> Enum.map(fn x -> String.to_float(x) end)
+    |> String.strip
+    |> String.split(",")
+    |> Enum.map(fn x -> String.to_float(x) end)
   end
 
   defp clean_lat_lon_string({:ok, %HTTPoison.Response{status_code: 404}}) do
--- mix.exs
+++ mix.exs
@@ -7,26 +7,30 @@ defmodule IpTools.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:httpoison, "~> 0.9.0"}]
+    [{:httpoison, "~> 0.11.1"}]
+  end
+
+  defp description do
+    """
+    A simple set of ip related tools focused on geolocation.
+    """
+  end
+
+  defp package do
+    [# These are the default files included in the package
+     name: :ip_tools,
+     maintainers: ["Chase Gilliam"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/Ch4s3/IPTools"}]
   end
 end
